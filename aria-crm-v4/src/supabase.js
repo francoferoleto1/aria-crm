25902a22eb0ab5810db99864164d53c9c56af2d5
francoferoleto1 <39317997+francoferoleto1@users.noreply.github.com>
2026-03-23 01:25:16 -0300

    Delete aria-crm-v4 directory

--- aria-crm-v4/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://rzbkuoabrhnalrtlveaf.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_z_b3RHZlQNuBrAUbZONfWQ_rpN28DAx'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
